a14e4053e53f5bdeb2825939a9dd6bda0c7918bb
Sergio <serxa92@hotmail.com>
2026-06-26 12:21:20 +0200

    Set up project architecture and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import CarDetail from "./pages/CarDetail/CarDetail";
+import Contact from "./pages/Contact/Contact";
+import Favorites from "./pages/Favorites/Favorites";
 
 const App = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cars/:id" element={<CarDetail />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/pages/CarDetail/CarDetail.jsx
@@ -0,0 +1,5 @@
+const CarDetail = () => {
+  return <h1>Car Detail</h1>;
+};
+
+export default CarDetail;
